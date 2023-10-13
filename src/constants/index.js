e99a49b611366b5367bb88aba378d88204b10667
moazabdulhalim <moazabdulhalim@gmail.com>
2023-10-13 08:59:44 +0300

    change api

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "http://localhost:3500/";
-// export const API_BASE_URL = "https://api.avarealestate.ae/";
+// export const API_BASE_URL = "http://localhost:3500/";
+export const API_BASE_URL = "https://api.avarealestate.ae/";
 
 export const Gender = ["Male", "Female"];
 export const Purpose = ["Rent", "Buy"];
